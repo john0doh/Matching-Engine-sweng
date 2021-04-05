caaf89a862e9cad43d8c68ec84eeef801c33fcab
john0doh <jod18045@gmail.com>
2021-04-05 21:36:23 +0100

    Added tolerances and relational queries

--- backend-api/routes/dbAPI.js
+++ backend-api/routes/dbAPI.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient
+var date = require('mongodb').Date
 
 var fs = require('fs');
 var password = fs.readFileSync("./routes/password.txt")
@@ -18,7 +19,7 @@ MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true })
 
     tradeCollection.findOne({},function(err, result) {
       initTypes(result)
-      //console.log(result)
+      console.log(types)
       
       //res.send(types)
     })
@@ -40,27 +41,49 @@ MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true })
 
     // Finds items which match {attr:val} exactly in the db
     // Only works for string equality at the moment
-    router.get("/match/:attr.eq.:val", function(req,res){
+    router.get("/match/:attr.:eq.:val", function(req,res){
       query = {}
+      eqtype = {}
       var attr = req.params.attr;
       var val = req.params.val
+      var eq = "$"+req.params.eq
+      
+      eqtype[eq] = "";
 
-      //console.log(types,attr,types[attr])
+      console.log(attr,types[attr])
 
-      if(types[attr].endsWith("Array")){
+      if(req.query.atol!=undefined && types[attr].startsWith("number")){
+        atol = Number(req.query.atol); 
+        tollow = Number(val)-atol;
+        tolhi = Number(val)+atol;
+        innerQuery = {$gte:tollow, $lte:tolhi};
+        query[attr] = innerQuery;
+      }
+      else if(req.query.rtol!=undefined && types[attr].startsWith("number")){
+        rtol = Number(req.query.rtol); 
+        tollow = Number(val)-(Number(val)*rtol);
+        tolhi = Number(val)+(Number(val)*rtol);
+        innerQuery = {$gte:tollow, $lte:tolhi};
+        query[attr] = innerQuery;
+      }
+      else if(types[attr].endsWith("Array")){
         if(types[attr].startsWith("number")){
-          query[attr] = [Number(val)]
+          eqtype[eq] = [Number(val)]
+          query[attr] = eqtype
         }
         else{
-          query[attr] = [val]
+          eqtype[eq] = val;
+          query[attr] = eqtype;
         }
       }
       else{
         if(types[attr].startsWith("number")){
-          query[attr] = Number(val)
+          eqtype[eq] = Number(val)
+          query[attr] = eqtype
         }
         else{
-          query[attr] = val
+          eqtype[eq] = val;
+          query[attr] = eqtype
         }
       }
       console.log(query)
@@ -79,8 +102,23 @@ MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true })
 // itinitalises the types variable to have key:type pairs recursivley
 function initTypes(result){
   for (attr in result){
+    
+    //regex for date
+    const regex = "\d\d\d\d-\d\d-\d\dT\d\d:\d\d.\d\d\dZ"
+
     if(Array.isArray(result[attr])){
-      types[attr] = "" + (typeof result[attr][0]) + " Array"
+      if(typeof result[attr][0] == "string"){
+        console.log(result[attr][0].match(regex));
+        if(result[attr][0].match(regex)!=null){
+          //is a date
+          types[attr] = "date Array"
+        }else{
+          //isnt a date
+          types[attr] = "" + (typeof result[attr][0]) + " Array"
+        }
+      }else{
+        types[attr] = "" + (typeof result[attr][0]) + " Array"
+      }
     }
     else{
       if(typeof result[attr] == "object" && attr != "_id"){
@@ -88,7 +126,19 @@ function initTypes(result){
         initTypes(result[attr])
       }
       else{
-        types[attr] = typeof result[attr]
+        //console.log(result[attr].match(regex))
+        if(typeof result[attr] == "string"){
+          console.log(result[attr].match(regex))
+          if(result[attr].match(regex)!=null){
+            types[attr] = "date";
+          }else{
+            //isnt a date
+            types[attr] = typeof result[attr]
+          }
+        }else{
+          //is a date
+          types[attr] = typeof result[attr]
+        }
       }
     }
   }
